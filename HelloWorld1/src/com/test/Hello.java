d38070f3a5ece28d518054dbf27a8531822a173c
Srinivas <srini806@gmail.com>
2017-12-16 21:38:49 +0530

    Message changed

--- HelloWorld1/src/com/test/Hello.java
+++ HelloWorld1/src/com/test/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello one");
 		System.out.println("Hello two");
-		System.out.println("Hello three");
+		System.out.println("Hello thre");
 
 	}
 
